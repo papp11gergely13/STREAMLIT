8270502dfa01a6108deb3ba4d12e74aa9068c148
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2023-11-29 14:07:13 +0100

    Update Magamtól.py

--- Magamtól.py
+++ Magamtól.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.write("2 hatványa")
 
-level = st.slider(".slider: Select the level", 0, 16)
+level = st.slider("2 hatványa", 0, 16)
 st.text('Selected: {}'.format(2**level))
