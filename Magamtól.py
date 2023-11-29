da82d8b44d7fc1164e503f3c03ad93d01adaf9b3
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2023-11-29 14:24:40 +0100

    Update Magamtól.py

--- Magamtól.py
+++ Magamtól.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.write("Hatványozás 2-vel")
 
 csuszka = st.slider("2 hatványa", 0, 16)
-st.text('Selected: {}'.format(2**csuszka))
+st.text('Eredmény: {}'.format(2**csuszka))
