3f1e566f06b5859c90dbb516511f33808b2e1e36
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2023-11-30 14:24:42 +0100

    Update Fügvények.py

--- Fügvények.py
+++ Fügvények.py
@@ -6,7 +6,7 @@ import streamlit as st
 def load_data():
     return pd.DataFrame(
         {
-            "first column": [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15 ],
+            "first column": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ],
             "second column": [10, 20, 30, 40],
         }
     )
