2e8f6f148ddc4086777532bca8e5e3a6ef4e68c4
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2023-11-30 14:10:36 +0100

    Update Fügvények.py

--- Fügvények.py
+++ Fügvények.py
@@ -1,7 +1,21 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+import streamlit as st
 
-df = pd.DataFrame(np.random.randn(10, 5), columns=("col %d" % i for i in range(5)))
+# Cache the dataframe so it's only loaded once
+@st.cache_data
+def load_data():
+    return pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40],
+        }
+    )
 
-st.table(df)
+# Boolean to resize the dataframe, stored as a session state variable
+st.checkbox("Use container width", value=False, key="use_container_width")
+
+df = load_data()
+
+# Display the dataframe and allow the user to stretch the dataframe
+# across the full width of the container, based on the checkbox value
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
