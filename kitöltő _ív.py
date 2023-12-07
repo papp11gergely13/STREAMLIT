794fb03fea64b0e98b7ea0413af851efbbb12a48
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2023-12-07 08:37:24 +0100

    Update kitöltő _ív.py

--- kitöltő _ív.py
+++ kitöltő _ív.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
-
-
-
 st.header(".header: This is a header") 
+
 name = st.text_input(".text_input: Enter Your name", "Type Here ...")
 name = st.text_input(".text_input: Enter Your name", "Type Here ...")
