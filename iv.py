9d438bffebc6981b6724dcd024670e4dba71edb0
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2023-12-07 09:07:41 +0100

    Update iv.py

--- iv.py
+++ iv.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.header(".header: This is a header") 
 
-name = st.text_input(".text_input: Enter Your name", "Type Here ...")
-email = st.text_input(".text_input: Enter Your name", "Type Here ...")
-csuszka = st.slider("2 hatványa", 0, 100)
+name = st.text_input("Enter Your name", "Type Here ...")
+email = st.text_input("Enter Your email", "Type Here ...")
+csuszka = st.slider("Életkorod ", 0, 100)
 status = st.radio(".radio: Select Gender: ", ('Male', 'Female'))
