ae67b915f40fcd1728757af1e5ce3657d1107b3c
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2023-12-07 08:43:17 +0100

    Update kitöltő _ív.py

--- kitöltő _ív.py
+++ kitöltő _ív.py
@@ -4,3 +4,5 @@ st.header(".header: This is a header")
 
 name = st.text_input(".text_input: Enter Your name", "Type Here ...")
 email = st.text_input(".text_input: Enter Your name", "Type Here ...")
+csuszka = st.slider("2 hatványa", 0, 100)
+status = st.radio(".radio: Select Gender: ", ('Male', 'Female'))
